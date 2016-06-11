f5db3546192fb8c11dcd0034c77643c753335de5
MisanthropicBit <alexander.asp.bock@gmail.com>
2016-06-11 18:39:39 +0200

    Made some refinements to the test runner

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,33 +3,37 @@
 __run_tests() {
     source test_functions.sh
 
-    local test_files=$(find . -name "*test*.sh" | grep -v -e "$0" -e "test_functions")
+    local test_files=($(find . -name "*test*.sh" -o -name "*test*.bash" | grep -v -e "$0" -e "test_functions.sh"))
 
     if [ -z "$test_files" ]; then
         __test_error "No tests collected" && exit 1
     fi
 
-    printf "\033[32m~>\033[0m Collected %d test(s)...\n" "${#test_files[@]}"
-
-    local passed=0
     local failed=0
 
-    while read -r f; do
-        source "$f"
-        __test
+    #trap 'printf "line number is %s" $LINENO' ERR
 
-        if [ $? -eq 0 ]; then
-            passed=$((passed + 1))
-        else
+    for f in ${test_files[@]}; do
+        source "$f"
+        local out=$(__test)
+
+        if [ -n "$out" ]; then
+            printf "\033[31m✗ $f:\033[0m\n$out\n"
             failed=$((failed + 1))
+        else
+            printf "\033[32m✓ $f\033[0m\n"
         fi
-    done <<< "$test_files"
+    done
+
+    printf "\n${#test_files[@]} test(s) collected, "
 
     if [ "$failed" -gt 0 ]; then
-        printf "\n\033[32m~>\033[0m %d test(s) \033[32mpassed\033[0m, %d test(s) \033[31mfailed\033[0m (%.2f %%)\n" "$passed" "$failed" "$(bc -l <<< "scale=2; $passed/$failed*100.0")"
-    else
-        printf "\033[32m~>\033[0m %b\n" "\033[32mAll tests passed\033[0m"
+        printf "%s failed (%.2f%%)\n" "$failed" "$(bc -l <<< "scale=2; $failed/${#test_files[@]}*100.0")"
+        exit 1
     fi
+
+    printf "0 failed\n"
+    exit 0
 }
 
 __run_tests
