8838c362119e248cdc2633901028fe2c6e62e127
MisanthropicBit <alexander.asp.bock@gmail.com>
2016-08-29 22:33:56 +0200

    Moved git functions into a separate file

--- .bash_powerprompt.sh
+++ .bash_powerprompt.sh
@@ -158,30 +158,6 @@ __bash_powerprompt() {
         fi
     }
 
-    # Returns 0 if the branch is clean, 1 otherwise
-    __is_git_branch_clean() {
-        2>/dev/null 1>&2 git status --ignore-submodules | grep 'nothing to commit'
-    }
-
-    # Returns 0 if the current directory is a git branch, 1 otherwise
-    __is_git_branch() {
-        2>/dev/null 1>&2 git status --ignore-submodules
-    }
-
-    # Get the current git branch. Use .git-prompt.sh if it is available
-    __get_current_git_branch() {
-        local git_branch=''
-
-        if type __git_ps1 > /dev/null; then
-            git_branch="$(__git_ps1 '%s')"
-        else
-            # This requires git v1.7+
-            git_branch=$(git rev-parse --abbrev-ref HEAD)
-        fi
-
-        printf "%s" "$git_branch"
-    }
-
     ######################################################################
 
     # Attempt to load the current theme
